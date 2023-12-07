16251ab3fa44b365c98b5b2d64da596512eafc03
aarricgra <aarricgra@alu.edu.gva.es>
2023-12-07 12:35:18 +0100

    1

--- src/main/java/org/example/ScriptLibreria.java
+++ src/main/java/org/example/ScriptLibreria.java
@@ -3,6 +3,7 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.sql.*;
+import java.util.Scanner;
 
 public class ScriptLibreria {
     static Connection connection;
@@ -25,9 +26,52 @@ public class ScriptLibreria {
             statement.executeUpdate(sql);
 
             doSelect();
+            doInsert();
+            System.out.println("*************************************+********************\n\n" +
+                    "Insert\n\n" +
+                    "*************************************+********************\n");
+            doSelect();
+            doUpdate();
+            System.out.println("*************************************+********************\n\n" +
+                    "Update\n\n" +
+                    "*************************************+********************\n");
+            doSelect();
+            doDelete();
+            System.out.println("*************************************+********************\n\n" +
+                    "Delete\n\n" +
+                    "*************************************+********************\n");
+            doSelect();
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    private static void doDelete() throws SQLException {
+        String sql = "delete from libros where titulo like ?";
+        PreparedStatement preparedStatement =connection.prepareStatement(sql);
+        preparedStatement.setString(1,"Ejemplo1");
+
+        preparedStatement.executeUpdate();
+    }
+
+    private static void doUpdate() throws SQLException {
+        String sql = "update libros set titulo = ? where titulo like ?";
+        PreparedStatement preparedStatement =connection.prepareStatement(sql);
+        preparedStatement.setString(1,"Ejemplo1");
+        preparedStatement.setString(2,"Ejemplo");
+
+        preparedStatement.executeUpdate();
+    }
+
+    private static void doInsert() throws SQLException {
+        String sql = "insert into libros (titulo,descripcion,autor,url) values (?,?,?,?)";
+        PreparedStatement preparedStatement =connection.prepareStatement(sql);
+        preparedStatement.setString(1,"Ejemplo");
+        preparedStatement.setString(2, "Ejemplo");
+        preparedStatement.setString(3, "Ejemplo");
+        preparedStatement.setString(4, "Ejemplo");
+
+        preparedStatement.executeUpdate();
     }
 
     private static void doSelect() throws SQLException {
